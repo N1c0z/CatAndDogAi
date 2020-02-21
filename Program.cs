c28c83e0c7d5f2a32e271d040c3d800444db52d7
Nico <Niccolo.caiti@gmail.com>
2020-02-21 19:54:26 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using CatAndDogAiML.Model;
+
+namespace CatAndDogAi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------\r\n| By Nico. | \r\n------------\r\n");
+            string answer;
+            bool DoWhile = true;
+            do
+            {
+                // Add input data
+                var input = new ModelInput();
+                Console.WriteLine("Insert file directory");
+                Console.Write("<user>:");
+                input.ImageSource = Convert.ToString(Console.ReadLine());
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.Write("\r\n");
+                Console.WriteLine($"Result:{result.Prediction}");
+                Console.WriteLine($"Continue? (Y/N)");
+                Console.Write("<user>:");
+                answer = Convert.ToString(Console.ReadLine());
+                
+                if (answer.ToLower() == "n")
+                {
+                    DoWhile = false;
+                }
+            } while (DoWhile);
+        }
+    }
+}
